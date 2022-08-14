29f38387202e99a7433db29e4b197ec1447d2a8f
Pavan Kumar Uppuluri <63893920+pavanuppuluri@users.noreply.github.com>
2022-08-14 14:23:57 +0530

    Create DivisionExample.java

---
+++ DivisionExample.java
@@ -0,0 +1,6 @@
+public class DivisionExample {
+    public static void main(String[] args) {        
+        System.out.println(10/0);     // ArithmeticException: / by zero
+        System.out.println(10.0/0.0); // Infinity 
+    } 
+}
