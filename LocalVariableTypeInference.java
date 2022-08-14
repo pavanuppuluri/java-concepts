5bce4259d91d00479f9ae4fab7cf5ee28bd0ccfe
Pavan Kumar Uppuluri <63893920+pavanuppuluri@users.noreply.github.com>
2022-08-14 09:36:33 +0530

    Local Variable Type Inference

---
+++ LocalVariableTypeInference.java
@@ -0,0 +1,6 @@
+class LocalVariableTypeInference {
+    public static void main(String[] args) {
+        var name = "Hi! Local Variable Type Inference";
+        System.out.println(name);
+    }
+}
